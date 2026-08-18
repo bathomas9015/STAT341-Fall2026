0fb2427e35843b9c5a2d7c91540dbc82fb377a87
bathomas9015 <bathomas@claflin.edu>
2026-08-18 16:13:50 -0400

    Descriptive-measures.r

---
+++ descriptive-measures.r
@@ -0,0 +1,22 @@
+#Name: Bailey Thomas, Date: 8/18/2026, Purpose: To test functions of descriptive statistics
+
+#Create a veriable called variable1 and store 5 number from 1-5
+
+veriable1<-c(1,2,3,4,5)
+[1] 1 2 3 4 5
+
+#Calculate mean of variable 1 
+mean(veriable1)
+[1] 3
+
+#Calculate standard devistion
+sd(veriable1)
+[1] 1.581139
+
+#Calculate median of variable 1 
+median(veriable1)
+[1] 3
+
+#Calculate the mode of variable 1 
+mode(veriable1)
+[1] "numeric"
