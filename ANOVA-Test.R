800c9df2c0db1bf7d433a038e7bbeffa651cff3c
bathomas9015 <bathomas@claflin.edu>
2026-09-10 16:15:15 -0400

    Add files via upload

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -14,3 +14,21 @@ df <- read.csv(PATH)
 #Plot the data using libary ggplot2
 ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
 
+#Apply one way ANOVA function on dependent veriable/time and independent variable/posion type
+anova_one_way <- aov(time~poison+treat, data = df); summary(anova_one_way)
+
+            Df Sum Sq Mean Sq F value
+poison       1 0.9316  0.9316   20.67
+Residuals   46 2.0735  0.0451        
+              Pr(>F)    
+poison      3.96e-05 ***
+Residuals               
+---
+Signif. codes:  
+  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1
+  ‘ ’ 1
+  ----------------------------------------------------------------------------------------
+  TukeyHSD(anova_one_way)
+
+
+
